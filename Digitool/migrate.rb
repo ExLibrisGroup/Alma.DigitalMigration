cb86c652d33e316ef6ace4f1e73a976ef19de2ff
exlcodeshare <api-team@exlibrisgroup.com>
2015-10-19 09:55:13 +0300

    Added: metadata file handling put files in separate folders on S3 (in case of duplicate filenames)
    Please review and merge.
    
    
    To do:
    Test with actual resumption token in OAI response
    Run import job
    Local clean-up
    Error handling

--- Digitool/migrate.rb
+++ Digitool/migrate.rb
@@ -47,11 +47,12 @@ s3_bucket = 'na-st01.ext.exlibrisgroup.com' # bucket name
 import_profile_id = '123456789' # ID of the import profile in ALma
 digitool_oai_base = 'http://dc03vg0053eu.hosted.exlibrisgroup.com:8881/OAI-PUB' # URL of OAI in digitool
 oai_set = 'foralma' 
-temp_folder = 'c:\temp\scratch\\'
+temp_folder = '/home/opherk/tmp/'
+marc_file = "#{temp_folder}marc.xml"
 ### <<<<<<<<<<<<<<<<
 
 log "Starting..."
-
+File.write("#{marc_file}", "<collection>", mode: 'w+')
 qs = "?verb=ListRecords&set=#{oai_set}&metadataPrefix=marc21"
 oai_ns = {'oai' => 'http://www.openarchives.org/OAI/2.0/', 'marc' => 'http://www.loc.gov/MARC21/slim'}
 
@@ -67,7 +68,7 @@ begin
 	log "#{recordCount} records retrieved"
 
 	# for each record
-	document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record', oai_ns)[0,1].each do |r| 
+	document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:record', oai_ns)[0,2].each do |r| 
 		identifier = r.at_xpath('oai:header/oai:identifier', oai_ns).content
 		identifier = identifier[identifier.rindex(':')+1..-1]
 
@@ -76,27 +77,33 @@ begin
 		r.xpath('oai:metadata/marc:record/marc:datafield[@tag="856"]/marc:subfield[@code="u"]', oai_ns).each_with_index do |f,i|
 			log "Downloading #{f.content}"
 			filename=download_file(f.content, "#{temp_folder}/#{identifier}")
-			filename="A study on the adoption and diffusion of information and communication technologies in the banking industry in Thailand using multiple-criteria decision making and system dynamics approaches..pdf"
+			#filename="A study on the adoption and diffusion of information and communication technologies in the banking industry in Thailand using multiple-criteria decision making and system dynamics approaches..pdf"
 			log "Saved #{temp_folder}#{identifier}/#{filename}"
 
 			log "Uploading to S3"
-			write_file(s3_bucket, "#{@inst}/upload/#{import_profile_id}/#{ingest_id}/#{filename}", "#{temp_folder}#{identifier}/#{filename}")
+			write_file(s3_bucket, "#{@inst}/upload/#{import_profile_id}/#{ingest_id}/#{identifier}/#{filename}", "#{temp_folder}#{identifier}/#{filename}")
 
 			# Update field
 			f.replace("#{identifier}/#{filename}")
-
+            
 			# TODO: delete file & folder (if empty)
 			#log "Uploaded. Removed file from temp location."
 
 		end
+        File.write("#{marc_file}", "#{r.xpath('oai:metadata/marc:record', oai_ns)}", mode: 'a')
 	end
 
-	# TODO: Write marcxml file to ingest location
-
+    File.write("#{marc_file}", "</collection>", mode: 'a')
+    
 	resumptionToken = 
 		document.xpath('/oai:OAI-PMH/oai:ListRecords/oai:resumptionToken', {'oai' => 'http://www.openarchives.org/OAI/2.0/'}).text	
 	qs = '?verb=ListRecords&resumptionToken=' + resumptionToken
-
+    
+    File.rename("#{marc_file}", "#{resumptionToken}#{marc_file}")
+	# Write marcxml file to ingest location
+    log "Uploading #{resumptionToken}#{marc_file} to S3 - uuid=#{ingest_id}"
+    write_file(s3_bucket, "#{@inst}/upload/#{import_profile_id}/#{ingest_id}/marc.xml", "#{marc_file}")
+    
 end until resumptionToken == ''
 
 log "Complete"
